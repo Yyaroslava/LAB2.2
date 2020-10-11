47bccb9e7523b2fc8c3bcf07f28e96cf390356f4
Yyaroslava <72552637+Yyaroslava@users.noreply.github.com>
2020-10-11 20:58:22 +0300

    Uncommented z2

--- lab2.2.cpp
+++ lab2.2.cpp
@@ -7,17 +7,17 @@ int main()
 {
     const double PI = 4 * atan(1.0); // число Пі
     double a;  // вхідний параметр 
-    //double z1; // результат обчислення 1-го виразу
+    double z1; // результат обчислення 1-го виразу
     double z2; // результат обчислення 2-го виразу 
 
     cout << "a =  "; cin >> a;// локальна змінна
 
-    //z1 = sin((PI / 2.0) + 3 * a) / (1 - sin(3 * a - PI));
+    z1 = sin((PI / 2.0) + 3 * a) / (1 - sin(3 * a - PI));
 
     z2 = 1 / tan((5.0 / 4.0) * PI + (3.0 / 2.0) * a);
 
     cout << endl;
-    //cout << "z1 = " << z1 << endl;
+    cout << "z1 = " << z1 << endl;
     cout << "z2 = " << z2 << endl;
 
     cin.get();
